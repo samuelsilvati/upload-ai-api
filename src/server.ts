423914ffb7f464f1ed9874b8be9855d7e95bb04e
Samuel Silva <samuel7silva@outlook.com>
2023-09-12 22:24:43 -0300

    init project

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import { fastify } from 'fastify'
+
+const app = fastify()
+
+app.get('/', () => {
+  return 'Hello World'
+})
+
+app
+  .listen({
+    port: 3333,
+    // host: '192.168.3.3',
+  })
+  .then(() => {
+    console.log('Server running on 192.168.2.3:3333')
+  })
