7567d0cdef700e4541d27987d673c226113c9e4c
Samuel Silva <samuel7silva@outlook.com>
2023-09-12 22:35:55 -0300

    install prisma and create models

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ app.get('/', () => {
 app
   .listen({
     port: 3333,
-    // host: '192.168.3.3',
+    host: '192.168.3.3',
   })
   .then(() => {
     console.log('Server running on 192.168.2.3:3333')
